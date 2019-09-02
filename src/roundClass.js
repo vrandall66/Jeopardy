5b2b07948d65214bb0b8e2fc1a76405537a04a63
Naomi Campos <naomicampos@nkcampos.local>
2019-09-01 18:56:14 -0600

    Create classes and build skeletons for methods

---
+++ src/roundClass.js
@@ -0,0 +1,49 @@
+//round 1 will need to instantiate daily double once
+//round 2 will have to instantiate daily double twice
+
+// class Round {
+//   constructor() {
+//     this.categories
+//     this.turnCounter = 0;
+//     this.clues = [];
+//     this.roundCategory;
+//     this.players = [];
+//     this.currentPlayer = 0;
+//     this.player1Score
+//     this.player2Score;
+//   }
+
+//   generateCategories() {
+//     rand nums * 4 
+//     this.categories.find(category => {
+//       category.ID === ID
+//     })
+//   }
+
+
+//   takeTurn() {
+//     this.turnCounter++
+//     OR change players 
+//   }
+
+//   evaluateGuess() {
+//     if answer includes ... then increase score 
+//     && change player
+//   }
+
+//   endRound() {
+//     this.turnCounter = 0
+//   }
+
+//   changePlayer() {
+//     this.currentPlayer++
+//   }
+
+//   generateBoard() {
+
+//   }
+
+
+
+
+// }
